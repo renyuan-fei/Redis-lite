faa4b66ffea186aeb1f551125dc8505500fd186e
Fry <renyuan.fei.1020@gmail.com>
2024-04-05 22:25:43 +1030

    Refactor server socket handling and rename project to Redis-lite
    The server code has been refactored to continuously accept new socket connections and handle each in a separate thread, improving concurrent client handling. Additionally, the entire project has been renamed from "codecrafters-redis" to "Redis-lite" reflecting both in solution and project files to better illustrate the project scope.

--- src/Server.cs
+++ src/Server.cs
@@ -11,16 +11,30 @@ Console.WriteLine("Logs from your program will appear here!");
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
 
-Socket socket = server.AcceptSocket();
-
-while (socket.Connected)
+while (true)
 {
-  byte[ ] requestData = new byte[1024];
-  socket.Receive(requestData);
-  byte[ ] responseData = Encoding.UTF8.GetBytes(responseTxt);
-  socket.Send(responseData);
+  // create a new socket instance
+  Socket socket = server.AcceptSocket();
+
+  // start a new thread to handle the socket
+  new Thread(() => HandleSocket(socket)).Start();
 }
 
-socket.Close();
+void HandleSocket(Socket socket)
+{
+  while (socket.Connected)
+  {
+    byte[] buffer = new byte[4096];
+    int bytesRead = socket.Receive(buffer);
 
-server.Stop();
+    // check if we got a valid response
+    if (bytesRead == 0) continue;
+
+    byte[ ] responseData = Encoding.UTF8.GetBytes(responseTxt);
+
+    socket.Send(responseData);
+
+    // close the socket after sending the response
+    socket.Close();
+  }
+}

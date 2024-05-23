bd7ac3e2d4aa1ae9f47e24904cb69ae16e82ae9b
Fry <renyuan.fei.1020@gmail.com>
2024-05-24 02:13:54 +0930

    Fix incorrect formatting in PSYNC command
    Fixed the incorrect backslash formatting in the PSYNC command being sent to the master Redis server. It no longer contains double backslashes ("\\"), and instead uses a single backslash ("\"). This ensures the proper command format and should help to avoid any potential communication issues.

--- src/RedisServer.cs
+++ src/RedisServer.cs
@@ -183,7 +183,7 @@ public class RedisServer
     await SendCommandToMaster("*1\r\n$4\r\nping\r\n");
     await SendCommandToMaster($"*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n{_port}\r\n");
     await SendCommandToMaster("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n");
-    await SendCommandToMaster("*3\\r\\n$5\\r\\nPSYNC\\r\\n$1\\r\\n?\\r\\n$2\\r\\n-1\\r\\n");
+    await SendCommandToMaster("*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n");
   }
 
   async private Task SendCommandToMaster(string request)

c356ac398ead0e5d3ea98e96b77b917bffb6addc
Fry <renyuan.fei.1020@gmail.com>
2024-05-24 02:10:16 +0930

    Update RedisServer to use string interpolation
    Changed the SendCommandToMaster method call in RedisServer.cs to utilize string interpolation for improved readability and maintainability. The modified string is related to configuration replication settings.

--- src/RedisServer.cs
+++ src/RedisServer.cs
@@ -181,7 +181,7 @@ public class RedisServer
     _tcpClientToMaster = new TcpClient(_masterHost, _masterPort);
 
     await SendCommandToMaster("*1\r\n$4\r\nping\r\n");
-    await SendCommandToMaster("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n{_port}\r\n");
+    await SendCommandToMaster($"*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n{_port}\r\n");
     await SendCommandToMaster("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n");
     await SendCommandToMaster("*3\\r\\n$5\\r\\nPSYNC\\r\\n$1\\r\\n?\\r\\n$2\\r\\n-1\\r\\n");
   }
